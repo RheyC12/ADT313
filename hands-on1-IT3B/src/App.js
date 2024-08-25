6c552930e35180ea7016483cb23076449dd56929
Rhey_Christian Verunque <rheychristianverunque1012@gmail.com>
2024-08-26 05:24:44 +0800

    first act

---
+++ hands-on1-IT3B/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './component/user/user';
+import Name from './component/name/name';
+import Section from './component/Section/Section';
+import Description from './component/Description/Description';
+
+
+function App() {
+  return (
+    <div className="App">
+       <Name />
+       <Section />
+       <Description />
+    </div> 
+  );
+}
+
+export default App;
